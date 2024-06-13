5a2d2a6a300aecdebda005cfafebbfe5dcb68c7e
Vlad-Petruk <vladpetruk00@gmail.com>
2024-06-13 19:07:21 +0300

    startes working on form section's content

---
+++ src/components/FormSection/FormBox.jsx
@@ -0,0 +1,9 @@
+
+
+function FormBox () {
+    return (
+        <></>
+    )
+}
+
+export default FormBox
--- src/components/FormSection/FormMainSection.jsx
+++ src/components/FormSection/FormMainSection.jsx
@@ -1,6 +1,21 @@
+import FormBox from './FormBox'
+import Button from '../Reusable Componets/Button'
+import './FormSection.css'
+
 function FormMainSection () {
     return (
-        <div>Fuck</div>
+        <div className="main-container">
+            <div className="buttons">
+                <Button text={'Clear example'} />
+                <Button text={'Load example'} />
+            </div>
+            <div className="form-boxes">
+                {/* use State in boxes to define its content showing */}
+                <FormBox />
+                <FormBox />
+                <FormBox />
+            </div>
+        </div>
     )
 }
 
