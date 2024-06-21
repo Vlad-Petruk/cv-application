b37e19fc862f1126e94355fae56d78c7d3ea00d5
Vlad-Petruk <vladpetruk00@gmail.com>
2024-06-21 20:15:06 +0300

    add Input component

--- src/components/Reusable Componets/Input.jsx
+++ src/components/Reusable Componets/Input.jsx
@@ -1,1 +1,12 @@
 import './ReusableContent.css'
+
+function Input ({type, id, label}) {
+    return (
+        <div className='inputBox'> 
+            <label htmlFor={id}>{label}</label>
+            <input type={type} id={id} className='input'></input>
+        </div>
+    )
+}
+
+export default Input
