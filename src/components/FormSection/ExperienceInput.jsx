3490d6f63746d1ff486d2667e9bcb9bd057920e0
Vlad-Petruk <vladpetruk00@gmail.com>
2024-06-27 17:41:41 +0300

    add functionality and state for experience section

--- src/components/FormSection/ExperienceInput.jsx
+++ src/components/FormSection/ExperienceInput.jsx
@@ -1,8 +1,50 @@
+import { useState } from "react";
 import Input from "../Reusable Componets/Input";
+import Button from "../Reusable Componets/Button";
+import InstitutionCard from "../Reusable Componets/InstitutionCard";
+import data from "../../data/cv";
 // in article Thinking in React are usefull information about passing the data upwards (step 5)
-function ExperienceInput () {
+function InputBox ({handleClick}) {
     return (
-        <Input />
+        <div className="input-box">
+            <Input type={'text'} id ={'company'} label={'Company'}/>
+            <Input type={'text'} id ={'positionTitle'} label={'Position Title'}/>
+            <Input type={'number'} id ={'startDate'} label={'Start date'}/>
+            <Input type={'number'} id ={'endDate'} label={'End date'}/>
+            <Input type={'text'} id ={'location'} label={'Location'}/>
+            <Input type={'text'} id ={'description'} label={'Description'}/>
+            <div className="buttons edu-btn">
+                <Button text={'Delete'} />
+                <Button text={'Save'} handleClick={handleClick}/>
+            </div>
+        </div>
+    )
+}
+
+function ExperienceInput () {
+    const [boxOpened, setBoxOpened] = useState(false)
+    function handleClick (id){
+        setBoxOpened(prevState => ({
+            ...prevState,
+            [id]: !prevState[id]
+        }));
+    }
+
+    function handleClickSave () {
+        setBoxOpened(prevState => !prevState)
+    }
+
+        
+    return (
+        <>
+        {data.professional_experience.map(experience => (
+            boxOpened[experience.key] ? (
+                <InputBox key={experience.key} handleClick={handleClickSave}/>
+            ) : (
+                <InstitutionCard key={experience.key} text={experience.company} handleClick={() => handleClick(experience.key)}/>
+            )
+        ))}
+        </>
     )
 }
 
