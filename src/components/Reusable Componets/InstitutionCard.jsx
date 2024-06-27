d8df07e3c2f371f65c3241a042e8a470691923e0
Vlad-Petruk <vladpetruk00@gmail.com>
2024-06-27 16:51:28 +0300

    add card component for education and experience sections

---
+++ src/components/Reusable Componets/InstitutionCard.jsx
@@ -0,0 +1,9 @@
+function InstitutionCard ({text, handleClick}) {
+    return (
+        <div className="institution-card" onClick={handleClick}>
+            {text}
+        </div>
+    )
+}
+
+export default InstitutionCard
