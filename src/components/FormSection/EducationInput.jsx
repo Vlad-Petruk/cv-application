7db587dbbde561d07d06565701d8ecf930fc07c1
Vlad-Petruk <vladpetruk00@gmail.com>
2024-07-01 07:38:33 +0300

    add static filing of education's input fields

--- src/components/FormSection/EducationInput.jsx
+++ src/components/FormSection/EducationInput.jsx
@@ -2,16 +2,15 @@ import { useState } from "react";
 import Input from "../Reusable Componets/Input";
 import Button from "../Reusable Componets/Button";
 import InstitutionCard from "../Reusable Componets/InstitutionCard";
-import data from "../../data/cv";
 // in article Thinking in React are usefull information about passing the data upwards (step 5)
-function InputBox ({handleClick}) {
+function InputBox ({handleClick, school, degree, startDate, endDate, location}) {
     return (
         <div className="input-box">
-            <Input type={'text'} id ={'school'} label={'School'}/>
-            <Input type={'text'} id ={'degree'} label={'Degree'}/>
-            <Input type={'number'} id ={'startDate'} label={'Start date'}/>
-            <Input type={'number'} id ={'endDate'} label={'End date'}/>
-            <Input type={'text'} id ={'location'} label={'Location'}/>
+            <Input type={'text'} id ={'school'} label={'School'} value={school}/>
+            <Input type={'text'} id ={'degree'} label={'Degree'} value={degree}/>
+            <Input type={'number'} id ={'startDate'} label={'Start date'} value={startDate}/>
+            <Input type={'number'} id ={'endDate'} label={'End date'} value={endDate}/>
+            <Input type={'text'} id ={'location'} label={'Location'} value={location}/>
             <div className="buttons edu-btn">
                 <Button text={'Delete'} />
                 <Button text={'Save'} handleClick={handleClick}/>
@@ -20,7 +19,7 @@ function InputBox ({handleClick}) {
     )
 }
 
-function EducationInput () {
+function EducationInput ({educations}) {
     const [boxOpened, setBoxOpened] = useState(false)
     function handleClick (id){
         setBoxOpened(prevState => ({
@@ -39,9 +38,9 @@ function EducationInput () {
         <div className="add-btn">
             <Button text={'+ Education'} />
         </div>
-        {data.education.map(education => (
+        {educations.map(education => (
             boxOpened[education.key] ? (
-                <InputBox key={education.key} handleClick={handleClickSave}/>
+                <InputBox key={education.key} handleClick={handleClickSave} school={education.school} degree={education.degree} startDate={education.startDate} endDate={education.endDate} location={education.location}/>
             ) : (
                 <InstitutionCard key={education.key} text={education.school} handleClick={() => handleClick(education.key)}/>
             )
