deaee13d0b02cff2080c12808e9ea9b5b43fe976
Vlad-Petruk <vladpetruk00@gmail.com>
2024-07-04 08:13:11 +0300

    add component to create new education card

---
+++ src/components/FormSection/NewEducationInput.jsx
@@ -0,0 +1,36 @@
+import {useState} from "react";
+import Input from "../Reusable Componets/Input";
+import Button from "../Reusable Componets/Button";
+
+function NewEducationInput({addEducation, handleStateClick}) {
+  const [school, setSchool] = useState('');
+  const [degree, setDegree] = useState('');
+  const [startDate, setStartDate] = useState('');
+  const [endDate, setEndDate] = useState('');
+  const [location, setLocation] = useState('');
+
+  function onSave() {
+    addEducation(school, degree, startDate, endDate, location);
+    handleStateClick();
+  }
+
+  return (
+    <div className="input-box">
+        <Input type={'text'} id ={'school'} label={'School'} value={school} onChange={setSchool}/>
+
+        <Input type={'text'} id ={'degree'} label={'Degree'} value={degree} onChange={setDegree}/>
+
+        <Input type={'number'} id ={'startDate'} label={'Start date'} value={startDate} onChange={setStartDate}/>
+
+        <Input type={'number'} id ={'endDate'} label={'End date'} value={endDate} onChange={setEndDate}/>
+
+        <Input type={'text'} id ={'location'} label={'Location'} value={location} onChange={setLocation}/>
+
+        <div className="buttons edu-btn">
+            <Button text={'Save'} handleClick={onSave}/>
+        </div>
+    </div>
+)
+}
+
+export default NewEducationInput;
